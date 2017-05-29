24ca92c53db40f27a93ec2048b8ef2e67744de78
samps01 <samps01@gmail.com>
2017-05-29 19:11:38 -0400

    Add post /todo route

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todos');
+const {Users} = require('./models/users');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{
+    const todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then((data)=>{
+        res.send(data);
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`);
+});
