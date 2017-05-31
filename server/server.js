3da2624284e7bf0fb2fc6dbe5bbdc86ef5537b07
samps01 <samps01@gmail.com>
2017-05-31 11:34:06 -0400

    add GET todo individual route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todos');
@@ -31,5 +32,20 @@ app.get('/todos',(req,res)=>{
    },(e)=>{
        res.status(400).send(e);
    });
+});
+
+app.get('/todos/:id',(req,res)=>{
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+          return res.status(404).send('Unable to find todo');
+        }
+        res.send({todo});
+    }).catch((err)=>{
+        res.status(400).send('Bad request');
+    })
 });
 module.exports={app};
