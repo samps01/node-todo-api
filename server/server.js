c5958631d5e318d4db0e5b5a9e8ca02d9989fa3a
samps01 <samps01@gmail.com>
2017-06-07 10:39:45 -0400

    add authenicate to individual route

--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ app.listen(port,()=>{
     console.log(`Server running on ${port}`);
 });
 
-app.post('/todos',(req,res)=>{
+app.post('/todos',authenticate,(req,res)=>{
     const todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator:req.user._id
     });
     todo.save().then((data)=>{
         res.send(data);
@@ -32,20 +33,25 @@ app.post('/todos',(req,res)=>{
 });
 
 
-app.get('/todos',(req,res)=>{
-   Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+   Todo.find({
+       _creator:req.user._id
+   }).then((todos)=>{
       res.send({todos})
    },(e)=>{
        res.status(400).send(e);
    });
 });
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
     const id = req.params.id;
     if(!ObjectID.isValid(id)){
         return res.status(404).send();
     }
-    Todo.findById(id).then((todo)=>{
+    Todo.findOne({
+       _id:id,
+        _creator:req.user._id
+    }).then((todo)=>{
         if(!todo){
           return res.status(404).send('Unable to find todo');
         }
