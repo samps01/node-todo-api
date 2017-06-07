be8ab103d7d51b7c1b04d0f238916e0c7f9ba10a
samps01 <samps01@gmail.com>
2017-06-07 11:08:51 -0400

    add config details

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
 console.log(`Env variable --- ${env}`)
+if(env === 'development' || env === 'test'){
+    const config  = require('./config.json');
+    const envConfig = config[env];
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    Object.keys(envConfig).forEach((key)=>{
+       process.env[key] = envConfig[key];
+    });
 }
